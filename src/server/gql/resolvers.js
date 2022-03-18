13890bdb90294166fce382c1b35aa5ee6911acca
Chris Koster <christopherjkoster@gmail.com>
2022-03-17 23:04:21 -0500

    Add persistent storage, gql schemas and resolvers

--- src/server/gql/resolvers.js
+++ src/server/gql/resolvers.js
@@ -1,3 +1,43 @@
+import storage from 'node-persist';
+import { v4 as uuid } from 'uuid';
+
+const STORAGE_KEY = 'shopping-list-store';
+
+storage.init({
+    dir: 'storage',
+    encoding: 'utf8'
+});
+
+const getStorage = async () => await storage.getItem(STORAGE_KEY) || [];
+const setStorage = (data) => storage.setItem(STORAGE_KEY, data);
+const findItemIndex = (items, id) => {
+    const index = items.findIndex(item => item.id === id);
+    if (index === -1) {
+        throw new Error(`Item ID ${id} was not found`);
+    }
+}
+
 export default {
-    test: () => 'test data'
+    addItem: async ({ item }) => {
+        const items = await getStorage();
+        const id = uuid();
+        items.push({ ...item, id });
+        setStorage(items);
+        return { ...item, id };
+    },
+    updateItem: async ({ id, item }) => {
+        const items = await getStorage();
+        const index = findItemIndex(items, id);
+        items.splice(index, 1, { ...item, id })
+        setStorage(items);
+        return items[index];
+    },
+    deleteItem: async ({ id }) => {
+        const items = await getStorage();
+        const index = findItemIndex(items, id);
+        items.splice(index, 1)
+        setStorage(items);
+        return id;
+    },
+    getItems: async () => await getStorage()
 };
--- src/server/gql/schema.js
+++ src/server/gql/schema.js
@@ -1,5 +1,26 @@
 export default `
-  type Query {
-    test: String
-  }
+    input ListItemInput {
+        name: String!
+        description: String
+        count: Int
+        completed: Boolean
+    }
+
+    type ListItem {
+        name: String!
+        description: String
+        count: Int
+        completed: Boolean
+        id: ID!
+    }
+
+    type Mutation {
+        addItem(item: ListItemInput): ListItem
+        updateItem(id: ID!, item: ListItemInput): ListItem
+        deleteItem(id: ID!): ID
+    }
+
+    type Query {
+        getItems: [ListItem]
+    }
 `;
