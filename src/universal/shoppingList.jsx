32f96a233612ea2ed286850b25743ec17195d7b9
Chris Koster <christopherjkoster@gmail.com>
2022-03-19 11:00:26 -0500

    Add styled-components, css reset and page header

--- src/universal/shoppingList.jsx
+++ src/universal/shoppingList.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.div`
+    color: #fefefe;
+    background: #4d81b7;
+    font-family: 'Dosis', sans-serif;
+    padding: 16px;
+    font-size: 15px;
+`
 
 export default () => (
     <>
-        <h1>Shopping list</h1>
+        <Header>SHOPPING LIST</Header>
     </>
 )
